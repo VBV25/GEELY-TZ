f493b9a1153d1497470db8f8f59b0df7514b3b13
VBV25 <vbv25boev@yandex.ru>
2023-06-18 03:43:16 +0300

    адаптив начало правок 16:00

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,38 +1,80 @@
-$(document).ready(function () {
-  /*-------Start------*/
-  $('.photo-slider').slick({
-    //------свойства слайдера-----
-    //---стрелки-
-    arrows: true,
-    //--количество отображаемых слайдов
-    slidesToShow: 3,
-    //--количество пролистываемых слайдов
-    slidesToScroll: 1,
-    //--скорость пролистывания
-    speed: 1000,
-    //--бесконечность!!!
-    infinite: true,
-    //--свайп мышкой и пальцем
-    draggable: true,
-    swipe: true,
-    touchThreshold: 3,
-    touchMove: true,
-    //--вертикальный слайдер
-    vertical: true,
-    verticalSwiping: true,
+$(document).ready(function() {
+    /*-------Start------*/
+    $('.photo-slider').slick({
+        //------свойства слайдера-----
+        //---стрелки-
+        arrows: true,
+        //--количество отображаемых слайдов
+        slidesToShow: 3,
+        //--количество пролистываемых слайдов
+        slidesToScroll: 1,
+        //--скорость пролистывания
+        speed: 1000,
+        //--бесконечность!!!
+        infinite: true,
+        //--свайп мышкой и пальцем
+        draggable: true,
+        swipe: true,
+        touchThreshold: 3,
+        touchMove: true,
+        //--вертикальный слайдер
+        vertical: true,
+        verticalSwiping: true,
+        //адаптивность
+        responsive: [{
+            breakpoint: 768.5,
+            settings: {
+                vertical: false,
+                verticalSwiping: false,
+            },
+        }, ],
+    });
     //
-  });
-  //
-  $('.car-slider').slick({
-    arrows: false,
-    dots: true,
-    appendDots: $('.car-slider__bullet'),
-    fade: true,
-    speed: 1000,
-    infinite: true,
-    draggable: true,
-    swipe: true,
-    touchThreshold: 3,
-    touchMove: true,
-  });
+    $('.car-slider').slick({
+        arrows: false,
+        dots: true,
+        appendDots: $('.car-slider__bullet'),
+        fade: true,
+        speed: 1000,
+        infinite: true,
+        draggable: true,
+        swipe: true,
+        touchThreshold: 3,
+        touchMove: true,
+    });
+
+    //-----мобильное-меню----
+    $('.navigation-block__menu-mobile').click(function() {
+        $('.navigation-block__menu').css('right', '0%');
+        $('body,html').addClass('hidden');
+    });
+    $('.navigation-block__model-mobile').click(function() {
+        $('.navigation-block__model').css('left', '0%');
+        $('body,html').addClass('hidden');
+    });
+    $('.navigation-block__btn-exit').click(function() {
+        $('.navigation-block__model').attr('style', '');
+        $('.navigation-block__menu').attr('style', '');
+        $('body,html').removeClass('hidden');
+    })
+
+
+    //---изменение-размера-экрана----
+    $(window).resize(function() {
+        //перезагрузка слайдера при изменении размера экрана
+        $('.photo-slider').slick('setPosition');
+        $('.car-slider').slick('setPosition');
+    });
+    //----------------------
+    $('.model-link').click(function() {
+        $('.model-link').removeClass('active-nav-link');
+        $(this).addClass('active-nav-link');
+        var textModelBtn = $(this).text();
+        $('.model-car-text').text(textModelBtn)
+    })
+    $('.menu-link').click(function() {
+        $('.menu-link').removeClass('active-nav-link');
+        $(this).addClass('active-nav-link');
+    })
+
 });
